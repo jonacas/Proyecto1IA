1db6d20941995463458686aca44787d3b594ad19
Jonatan <al315616@uji.es>
2017-10-05 22:30:17 +0200

    PlayerMovement
    Falta ajustar el giro al moverse

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    private float moveX;
+    private float moveY;
+    private float rotateSpeed = 0.9f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        ReadInputs();
+        MovePlayer();
+	}
+
+    void MovePlayer()
+    {
+        transform.Translate(moveX, 0, moveY);
+        /*if (moveY < 0 && transform.localEulerAngles.y > -90f) {
+
+            transform.localRotation = new Vector3(0, transform.localEulerAngles.y - rotateSpeed, 0);
+        }
+        else if (moveY > 0 && transform.localEulerAngles.y < 90f) {
+
+            transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + rotateSpeed, 0);
+
+        }*/
+    }
+
+    void ReadInputs() {
+
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+        
+
+    }
+}
