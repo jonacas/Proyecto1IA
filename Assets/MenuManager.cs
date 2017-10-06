b8b734208a1b0aaaf683362d9f02599aa8310213
Albert Dols Ferrandis <al315151@uji.es>
2017-10-06 18:42:59 +0200

    Placeholder de Menu Principal, y esenario nais
    Separados en dos objetos distintos el cristal y la pared del escenario,
    y creado un placeholder para los nuevos botones del menú principal.

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void StartGame()
+	{
+		print ("Inisia el joigo bonito xdxd");
+	}
+
+
+
+	public void QuitGame()
+	{
+		print ("Hasta luego maderfaquer");
+		Application.Quit ();
+	}
+
+
+
+}
