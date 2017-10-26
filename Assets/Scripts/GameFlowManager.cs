fa37c1aa636f50f596ec8736442b17bc7eda619a
Albert Dols Ferrandis <al315151@uji.es>
2017-10-26 09:27:53 +0200

    Pantalla Fin de Juego creada
    Falta que sea activada por lo que sea... (y si hacemos mainMenu, que lleve a esa escena, pero vamos es una linea)

---
+++ Assets/Scripts/GameFlowManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameFlowManager : MonoBehaviour {
+
+
+	public GameObject endGameCanvas;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		endGameCanvas.SetActive (false);		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.T)) 
+		{
+			endGameCanvas.SetActive(true);
+			Time.timeScale = 0f;
+		}
+		if (Input.GetKeyDown (KeyCode.P)) 
+		{
+			endGameCanvas.SetActive(false);
+			Time.timeScale = 1f;
+		}
+		
+	}
+
+	 public void RestartLevel()
+	{
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+		endGameCanvas.SetActive (false);
+		Time.timeScale = 1f;
+	}
+
+	 public void QuitToMainMenu()
+	{
+		//Aqui cambiaremos el codigo de arriba, pero con la escena del menu.
+		
+	}
+
+
+
+}
