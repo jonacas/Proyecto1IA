b13c93c5e0c2854617b1ead7852c213c1e2aa9f3
al286368 <al286368@uji.es>
2017-10-27 16:54:43 +0200

    Animacion de introduccion al nivel
    - Es un prefab, arrastradlo al canvas del nivel, utiliza anchors asi que deberia ajustarse a la resolucion, para iniciar la animacion, llamad de forma externa a la funcion PlayIntroAnimation, o simplemente llamadla desde Start()

---
+++ Assets/LevelIntroAnimation.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelIntroAnimation : MonoBehaviour {
+
+	public string levelNameDisplayedString;
+	public string levelDescDisplayedString;
+
+	public Text levelName;
+	public Text levelDesc;
+	public Image introLine;
+	public CanvasGroup introCG;
+
+	private Vector3 levelNameInitialPos;
+	private Vector3 levelDescInitialPos;
+	private Vector3 separationLineInitialPos;
+
+	// Use this for initialization
+	void Awake () {
+		levelNameInitialPos = levelName.transform.localPosition;
+		levelDescInitialPos = levelDesc.transform.localPosition;
+		separationLineInitialPos = introLine.transform.localPosition;
+		levelName.text = levelNameDisplayedString;
+		levelDesc.text = levelDescDisplayedString;
+	}
+	IEnumerator LevelIntro()
+	{
+		introCG.alpha = 0;
+		float animSpeed = 1.85f;
+		float t = 0;
+		yield return new WaitForSeconds (0.5f);
+		while (t < 1) {
+			t = Mathf.MoveTowards (t, 1, Time.deltaTime * animSpeed);
+			introCG.alpha = t;
+			levelName.transform.localPosition = levelNameInitialPos + Vector3.right * (1 - t) * 40;
+			levelDesc.transform.localPosition = levelDescInitialPos + Vector3.left * (1 - t) * 40;
+			yield return null;
+		}
+		yield return new WaitForSeconds (2f);
+		while (t > 0) {
+			t = Mathf.MoveTowards (t, 0, Time.deltaTime * animSpeed);
+			introCG.alpha = t;
+			levelName.transform.localPosition = levelNameInitialPos + Vector3.left * (1 - t) * 40;
+			levelDesc.transform.localPosition = levelDescInitialPos + Vector3.right * (1 - t) * 40;
+			yield return null;
+		}
+	}
+
+	public void PlayIntroAnimation()
+	{
+		StartCoroutine ("LevelIntro");
+	}
+}
