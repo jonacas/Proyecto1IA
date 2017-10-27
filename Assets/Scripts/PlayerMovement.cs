0cf795e132ed19aebc2aa56c590f9ae6560f20bc
Jonatan <al315616@uji.es>
2017-10-27 15:45:28 +0200

    Arreglillos de media tarde
    +Movimiento del personaje relativo a la camara.
    +Intento de arreglar el z-buffer

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,14 +5,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    public float speed;
 	public GameObject camera;
     private float moveX;
     private float moveY;
     private Quaternion targetRotation;
     public GameObject child;
-    private const float RotationSpeed = 380f;
-	private const float moveSpeed = 10;
+    private const float RotationSpeed = 450f;
+	public float moveSpeed = 10;
 	private Rigidbody RB;
     public GameCamera cameraScript;
     private float offset;
@@ -40,7 +39,10 @@ public class PlayerMovement : MonoBehaviour {
 
     void MovePlayer()
     {
-		transform.Translate(moveX * Time.fixedDeltaTime * moveSpeed, 0, moveY * Time.fixedDeltaTime * moveSpeed);
+        Vector3 currentTranslation = new Vector3(moveX, 0, moveY) * Time.fixedDeltaTime * moveSpeed;
+        Quaternion camAdjustedRotation = Quaternion.Euler(0, camera.transform.rotation.eulerAngles.y, 0);
+        currentTranslation = camAdjustedRotation * currentTranslation;
+		transform.Translate(currentTranslation);
 		child.transform.rotation = Quaternion.RotateTowards(child.transform.rotation,targetRotation,RotationSpeed * Time.fixedDeltaTime);
 
     }
@@ -64,30 +66,30 @@ public class PlayerMovement : MonoBehaviour {
 
             if (moveY > 0)
             {
-				targetRotation =  Quaternion.Euler(0, -45 , 0); // Era (0, 45, 0)
+				targetRotation = cameraRotation *  Quaternion.Euler(0, -45 , 0); // Era (0, 45, 0)
             }
             else if (moveY < 0)
             {
-				targetRotation = Quaternion.Euler(0, 45 , 0);// Era (0,135, 0)
+				targetRotation = cameraRotation * Quaternion.Euler(0, 45 , 0);// Era (0,135, 0)
             }
             else
             {
-				targetRotation =  Quaternion.Euler(0, 0 , 0); // Era (0, 90, 0)
+				targetRotation = cameraRotation * Quaternion.Euler(0, 0 , 0); // Era (0, 90, 0)
             }
         }
         else if (moveX < 0) // Se mueve en Y-
         {
             if (moveY > 0)
             {
-				targetRotation =  Quaternion.Euler(0, 225 , 0); // Era (0, 315, 0)
+				targetRotation = cameraRotation * Quaternion.Euler(0, 225 , 0); // Era (0, 315, 0)
             }
             else if (moveY < 0)
             {
-				targetRotation =   Quaternion.Euler(0, 135 , 0); // Era (0, 225, 0)
+				targetRotation = cameraRotation * Quaternion.Euler(0, 135 , 0); // Era (0, 225, 0)
             }
             else
             {
-				targetRotation =   Quaternion.Euler(0, 180 , 0); // Era (0,270, 0)
+				targetRotation = cameraRotation * Quaternion.Euler(0, 180 , 0); // Era (0,270, 0)
 
             }
         }
@@ -95,11 +97,11 @@ public class PlayerMovement : MonoBehaviour {
             if (moveY > 0)
             {
 
-                targetRotation =  Quaternion.Euler(0, -90 , 0); // Era (0,0,0)
+                targetRotation = cameraRotation * Quaternion.Euler(0, -90 , 0); // Era (0,0,0)
             }
             else if (moveY < 0)
             {
-                targetRotation =  Quaternion.Euler(0, 90 , 0); // Era (0, 180, 0)
+                targetRotation = cameraRotation * Quaternion.Euler(0, 90 , 0); // Era (0, 180, 0)
             }
             else {
                 targetRotation = cameraRotation * Quaternion.Euler(0,-90, 0);
