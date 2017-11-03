b023fe147d9de9c7be6b429c9840d1f46d486c15
al315171 <32774606+al315171@users.noreply.github.com>
2017-11-03 17:12:49 +0100

    Add files via upload

---
+++ Assets/Scripts/SecurityCameras.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecurityCameras : MonoBehaviour {
+
+	const int STATE_STOP = 0, STATE_ROTATING = 1;
+
+	public float timeCameraIsStopped = 1.5f, rotationSpeed = 15f, maxAngleRotation = 50f;
+	float state, aux/* count time */;
+	Vector3 originalForward;
+
+	void Awake()
+	{
+		state = STATE_ROTATING;
+		originalForward = transform.forward;
+	}
+	void Update () {
+		rotate ();
+	}
+
+	void rotate(){
+		if ((Vector3.Angle (transform.forward, originalForward) > maxAngleRotation) && (aux == 0 || (state == STATE_STOP))) {
+			state = STATE_STOP;
+			aux += Time.deltaTime;
+			if (aux > timeCameraIsStopped) {
+				state = STATE_ROTATING;
+				rotationSpeed = -rotationSpeed;
+			}
+		}
+		if (state == STATE_ROTATING) {
+			if (aux != 0 && Vector3.Angle (transform.forward, originalForward) < maxAngleRotation)
+				aux = 0;
+			transform.Rotate (Vector3.up, Time.deltaTime * rotationSpeed); 
+		}
+	}
+}
