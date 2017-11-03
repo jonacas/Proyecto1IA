e5edc44b27cb3b5a6d2b5b52531f1d98b6d248f1
Jonatan <al315616@uji.es>
2017-11-03 13:33:22 +0100

    TextGenatorBeta
    +Text Generator esta en beta, ahora mismo no funciona, se han encontrado
    diversos problemas. A la hora de usar dos personajes en escena

---
+++ Assets/Scripts/TextGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextGenerator : MonoBehaviour {
+
+    public Text texto;
+    public string[] patrulla = new string[6];
+    public string[] Informar = new string[6];
+    public string[] alerta = new string[6];
+    public string[] presiguiendo = new string[6];
+    public bool isAlerta = false;
+    public bool isInformar = false;
+    public bool isPersiguiendo = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (isAlerta)
+        {
+
+            int i = Random.Range(0, 5);
+            texto.text = alerta[i];
+
+        }
+        else if (isInformar) {
+
+            int i = Random.Range(0, 5);
+            texto.text = Informar[i];
+
+        }
+        else if (isPersiguiendo)
+        {
+
+            int i = Random.Range(0, 5);
+            texto.text = presiguiendo[i];
+
+        }
+
+    }
+}
