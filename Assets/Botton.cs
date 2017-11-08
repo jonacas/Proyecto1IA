5df55f127dcc6e898a0ab787da1b604584bac0fc
Jonatan <al315616@uji.es>
2017-11-08 12:57:40 +0100

    BottonPulsable
    +Se ha añadido el prefab del Botton Pulsable

---
+++ Assets/Botton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Botton : MonoBehaviour {
+
+    public Animator anim;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown("e")) { anim.enabled = true; }
+        
+    }
+}
