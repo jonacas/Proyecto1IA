58926faf5af642ac53151c5d2386fc6ec0bdf08d
Albert Dols Ferrandis <al315151@uji.es>
2017-11-08 08:51:21 +0100

    A TRAVES DE LAS PAREDES
    Ahora, si la camara no ve al player, cambia el alpha del material de paredes para que se vea.
    EZ PZ LEMON SQUEEZY.

--- Assets/Scripts/GameCamera.cs
+++ Assets/Scripts/GameCamera.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameCamera : MonoBehaviour {
 
-	public GameObject camera;
+	public GameObject cameraObject;
 	public GameObject player;
 	public float cameraAngle = 0f;
 	private Vector3 previousMousePosition;
@@ -13,6 +13,11 @@ public class GameCamera : MonoBehaviour {
 	[Range(0, 200)]
 	public float cameraSpeed;
 	private float previousMouseDifference;
+
+	public Material[] wallMaterial;
+	[Range(0.3f, 1f)]
+	public float wallMaterialAlpha = 1f;
+
 
 	// Use this for initialization
 	void Start () {
@@ -24,10 +29,16 @@ public class GameCamera : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		camera.transform.LookAt (player.transform);
+
+		CheckPlayerVisible ();
+
+		wallMaterial[0].color = new Color(wallMaterial[0].color.r, wallMaterial[0].color.g, wallMaterial[0].color.b, wallMaterialAlpha);
+		wallMaterial[1].color = new Color (wallMaterial [1].color.r, wallMaterial [1].color.g, wallMaterial [1].color.b, wallMaterialAlpha);
+
+		cameraObject.transform.LookAt (player.transform);
 		//CalculateMousePosition ();
 		differenceCameraPositionFromCenter();
-		camera.transform.RotateAround(player.transform.position, new Vector3 (0.0f, 1.0f, 0.0f), Time.deltaTime * cameraAngle);
+		cameraObject.transform.RotateAround(player.transform.position, new Vector3 (0.0f, 1.0f, 0.0f), Time.deltaTime * cameraAngle);
 	}
 
 	void differenceCameraPositionFromCenter()
@@ -55,6 +66,27 @@ public class GameCamera : MonoBehaviour {
 		else { cameraAngle = 0f;		}
 		//Reiniciamos valores para siguiente frame
 		previousMousePosition = mouseInputDifferenceFromCenter;
+
+	}
+
+	void CheckPlayerVisible()
+	{
+		RaycastHit hit;
+		Vector3 playerRayCastCoordinate = cameraObject.GetComponent<Camera> ().WorldToScreenPoint (player.transform.position);
+		Ray newRay = cameraObject.GetComponent<Camera> ().ScreenPointToRay (playerRayCastCoordinate);
+		//Debug.DrawRay (newRay.origin, newRay.direction * 50, Color.yellow);
+		if (Physics.Raycast (newRay, out hit)) 
+		{
+			if (hit.transform.tag != "Player") 
+			{
+				wallMaterialAlpha = 0.75f;
+			} 
+			else
+			{
+				wallMaterialAlpha = 1f;
+			}
+		}
+
 
 	}
 
